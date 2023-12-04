3be67c9fbc0f24adfe2167ac8e73e35d339b1aaf
danielmegale <danim1@live.it>
2023-12-04 17:08:35 +0100

    add create page

--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -7,9 +7,14 @@ import org.java.spring.db.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class MainController {
@@ -26,6 +31,25 @@ public class MainController {
 		model.addAttribute("q", q == null ? "" : q);
 
 		return "Pizzas";
+	}
+
+	@GetMapping("/pizzas/create")
+	public String createPizza(Model model) {
+
+		Pizza pizza = new Pizza();
+		model.addAttribute("pizza", pizza);
+		return "pizza-form";
+	}
+
+	@PostMapping("/pizzas/create")
+	public String storePizza(Model model,@Valid @ModelAttribute Pizza pizza, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()){
+			model.addAttribute("pizza", pizza);
+			return "pizza-form";
+		}
+		pizzaService.save(pizza);
+		return "redirect:/";
+		
 	}
 
 	@GetMapping("/pizzas/{id}")
--- src/main/java/org/java/spring/db/pojo/Pizza.java
+++ src/main/java/org/java/spring/db/pojo/Pizza.java
@@ -1,10 +1,15 @@
 package org.java.spring.db.pojo;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 @Entity
 public class Pizza {
@@ -13,18 +18,24 @@ public class Pizza {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
-	@Column(nullable = false, unique = true)
+	@Column(nullable = false)
+	@Length(min=3,max=60,message = "Il nome deve avere minimo 3 caratteri e non più di 60")
 	private String nome;
-	@Column(nullable = false, unique = true)
+	
+	@Column(nullable = false)
+	@Length(min=3,max=140,message = "La descrizione deve avere minimo 3 caratteri e non più di 140")
 	private String descrizione;
-	@Column(nullable = false, unique = false)
+	
+	@Column(nullable = false)
+	@Range(min=0,max=100,message="Il prezzo deve avere un valora compreso tra 0.1 a 100"+"\n")
+	@NotNull(message="Il prezzo non puo essere 0")
+	@Positive(message="Il prezzo deve essere positivo")
 	private double prezzo;
-
-	@Column(unique = true)
+	
+	@Column(unique = false)
 	private String foto;
 
-	public Pizza() {
-	}
+	public Pizza() {}
 
 	public Pizza(String nome, String descrizione, double prezzo, String foto) {
 		setNome(nome);
